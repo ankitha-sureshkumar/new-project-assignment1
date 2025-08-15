b61106dfd928905c5c0604e9189ab3cf05540afc
ankitha-sureshkumar <ankithasuresh1058@gmail.com>
2025-08-15 22:13:40 +1000

    adding app.js and remove commented lines

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
+import Pets from './pages/Pets';
+import Home from './pages/Home';
+import BookAppointment from './pages/BookAppointment';
 
 function App() {
   return (
@@ -13,7 +16,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/pets" element={<Pets />} />
         <Route path="/tasks" element={<Tasks />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/book-appointment" element={<BookAppointment />} />
       </Routes>
     </Router>
   );
