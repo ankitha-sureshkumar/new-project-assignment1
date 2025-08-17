69e716222c5ac40330ccb9d0c24d046ee88f018f
ankitha-sureshkumar <ankithasuresh1058@gmail.com>
2025-08-17 14:52:01 +1000

    public ip address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.210.154.142:5001', // live
+  //baseURL: 'http://localhost:5000', // local
+  baseURL: 'https://3.25.193.154:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
