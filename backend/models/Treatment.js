f12b7f930e151d2fe358dead2d9b1e87ab4789f8
ANKITHA SURESH KUMAR <ankithasuresh1058@gmail.com>
2025-10-02 16:36:45 +1000

    commit conflicts

--- backend/models/Treatment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const treatmentSchema = new mongoose.Schema({
-  petId: { type: mongoose.Schema.Types.ObjectId, ref: "Pet", required: true },
-  description: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Treatment", treatmentSchema);
